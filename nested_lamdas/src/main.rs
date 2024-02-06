bc0fd74e74f8b28c3e6b58246e360f03eb3d8828
Hideki A. Ikeda <hidekiai+hidekiai-lt@CodeMonkeyNinja.dev@users.noreply.github.com>
2024-02-06 16:36:02 -0600

    new update, this time for real on the for() loop using Option.iter()

--- nested_lamdas/src/main.rs
+++ nested_lamdas/src/main.rs
@@ -13,64 +13,77 @@
 //   lambda will only get triggered if the outer lambda has a result
 
 fn main() {
-    let arry1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let arry2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-    let possible_result = Some(vec![42, 86]);   // Yes I know, why do Option<Vec<T>> when you can assume None to mean empty array and Some to mean array of 1 or more...
-    let empty_result: Option<Vec<i32>> = Some(vec![]);
+    let arry1 = vec![42, 86];
+    let arry2 = vec![];
+    let possible_result = Some(arry1); // Yes I know, why do Option<Vec<T>> when you can assume None to mean empty array and Some to mean array of 1 or more...
+    let empty_result: Option<Vec<i32>> = Some(arry2);
     let no_result: Option<Vec<i32>> = None;
 
     // demo 1: loop based
     // NOTE: foo is read-only, in functional programming, it's a good practice not pass mutable reference
-    let my_lambda = |possible_foo: &Option<Vec<i32>>| {
-        if let Some(foo) = possible_foo {
-            print!("foo is valid...");
-            // Only enters this block if possible_foo is Some
-            for result in foo {
-                // ... do something with result
-                print!("{} ", result);
+    // this is because Option::iter() (https://doc.rust-lang.org/std/option/enum.Option.html#method.iter)
+    // can flatten the Option<T> into 0 or 1 elements
+    let my_lambda = |possible_foo: &Option<Vec<i32>>| -> Vec<String> {
+        let mut ret: Vec<String> = vec![]; // need to return an instance of string rather than &str
+        for result1 in possible_foo.iter() {
+            ret.push("foo is valid...".into());
+            for result2 in result1 {
+                ret.push(format!("{}", result2).into());
             }
-            println!();
-        } else {
-            // possible_foo is None, so no need to enter the loop
-            println!("No data available.");
         }
+        ret
     };
 
     // Output:
-    // foo is valid...42 86
-    // foo is valid...
-    // No data available.
-    let result1 = my_lambda(&possible_result);   // should list the i32s
-    let result2 = my_lambda(&empty_result);       // should print "foo is valid", but no i32's will be printed
+    // Result 1: ["foo is valid... ", "42 ", "86 "]
+    // Result 2: ["foo is valid... "]
+    // Result 3: []
+    let result1 = my_lambda(&possible_result); // should list the i32s
+    let result2 = my_lambda(&empty_result); // should print "foo is valid", but no i32's will be printed
     let result3 = my_lambda(&no_result); // should print "No data available."
-
+    println!("Result 1: {:?}", result1);
+    println!("Result 2: {:?}", result2);
+    println!("Result 3: {:?}", result3);
 
     // demo 2: lambda based
     // trick here is to use return for each lambdas;
     // in this example, we'll return the accumulated log string
     // NOTE: foo is read-only, in functional programming, it's a good practice not pass mutable reference
     let my_lambda2 = |possible_foo: &Option<Vec<i32>>| -> Vec<String> {
-        return if let Some(foo) = possible_foo {
-            let mut log = vec![];
-            log.push(format!("foo is valid... "));
-            // Only enters this block if possible_foo is Some
-            for result in foo {
-                // ... do something with result
-                log.push(format!("{} ", result));
+        let ret = match possible_foo {
+            Some(foo) => {
+                let mut ret: Vec<String> = vec![];
+                ret.push("foo is valid...".into());
+                for result in foo {
+                    ret.push(format!("{}", result).into());
+                }
+                ret
             }
-            log
-        } else {
-            // possible_foo is None, so no need to enter the loop
-            vec![String::from("No data available.")]
-        }
+            None => vec!["No data available.".into()],
+        };
+        ret
+    };
+    let my_lambda3 = |possible_foo: &Option<Vec<i32>>| -> Vec<String> {
+        let ret = match possible_foo {
+            Some(foo) => {
+                let mut ret: Vec<String> = vec![];
+                ret.push("foo is valid...".into());
+                for result in foo {
+                    ret.push(format!("{}", result).into());
+                }
+                ret
+            }
+            None => vec!["No data available.".into()],
+        };
+        ret
     };
 
-    // output: 
+    // output:
     // Result 4: ["foo is valid... ", "42 ", "86 "]
     // Result 5: ["foo is valid... "]
     // Result 6: ["No data available."]
-    let result4 = my_lambda2(&possible_result);   // should list the i32s
-    let result5 = my_lambda2(&empty_result);       // should print "foo is valid", but no i32's will be printed
+    let result4 = my_lambda2(&possible_result); // should list the i32s
+    let result5 = my_lambda2(&empty_result); // should print "foo is valid", but no i32's will be printed
     let result6 = my_lambda2(&no_result); // should print "No data available."
     println!("Result 4: {:?}", result4);
     println!("Result 5: {:?}", result5);
