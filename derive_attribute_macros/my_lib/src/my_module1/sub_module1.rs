18a3b48837c23ea0e3c346f91e84ef97fac1e6d1
Hideki A. Ikeda <hidekiai+hidekiai-lt@CodeMonkeyNinja.dev@users.noreply.github.com>
2024-02-09 14:28:27 -0600

    made sure to demonstrate how the compiler (and static-analyzer) can catch the macro design

--- derive_attribute_macros/my_lib/src/my_module1/sub_module1.rs
+++ derive_attribute_macros/my_lib/src/my_module1/sub_module1.rs
@@ -1,9 +1,10 @@
-use my_macros::{make_answer, my_macro1, my_proc_macro};
+use my_macros::{make_answer, my_macro1, MyProcMacro};
 
 // deriving proc-macro impl
-#[derive(my_proc_macro)]
+#[derive(MyProcMacro)]
 struct MyStruct {
     my_field: u32,
+    my_var1: f32, // NOTE: Try commenting this out, you should get the compiler warning that 'my_var1' does not exist, and if you change this to i32 (or any other type than f32), you will also get the message defined in the proc-macro
 }
 
 // calling proc-macro directly
@@ -12,4 +13,15 @@ pub(crate) fn do_something() {
     // construct a 'fn answer()' function:
     make_answer!();
     println!("The answer is: {}", answer());
-}   
+
+    // the MyProcMacro should have a method 'half_my_var1' that halves the value of 'my_var1'
+    // first, dump my_var1 to verify its value
+    let mut my_struct = MyStruct {
+        my_field: 42,
+        my_var1: 3.14,
+    };
+    println!("my_var1: {}", my_struct.my_var1);
+    // then, call the method to halve my_var1
+    my_struct.half_my_var1();
+    println!("half_my_var1: {:?}", my_struct.my_var1);
+}
--- derive_attribute_macros/my_macros/src/lib.rs
+++ derive_attribute_macros/my_macros/src/lib.rs
@@ -12,7 +12,6 @@ extern crate proc_macro;
 //use my_module2;
 //pub use my_module2::my_macro;
 
-
 // Interesting concept based on error:
 // > error: functions tagged with `#[proc_macro]` must currently reside in the root of the crate
 //#[proc_macro_derive]
@@ -24,35 +23,65 @@ pub fn my_macro1(_input: TokenStream) -> TokenStream {
     TokenStream::new()
 }
 
-
 // Example usage:
 //      extern crate proc_macro_examples;
 //      use proc_macro_examples::make_answer;
-//      
+//
 //      make_answer!();
-//      
+//
 //      fn main() {
 //          println!("{}", answer());
 //      }
 #[proc_macro]
 pub fn make_answer(_item: TokenStream) -> TokenStream {
     // just like traditional C/C++ macro, a "macro" in this case
-    // will place this string IN PLACE of the macro call as 
+    // will place this string IN PLACE of the macro call as
     // a function "fn answer()"
     "fn answer() -> u32 { 42 }".parse().unwrap()
 }
 
-// I want to define derive macros in a submodule, note that I would declare 
-// it as 'my_proc_macro' but the method-function is 'my_proc_macro_fn'
-// just to show that the name of the function doesn't need to match the name of the macro
-#[proc_macro_derive(my_proc_macro)]
+// Purpose of this macro:
+// verifies that the struct has a field named `my_var1` of type `f32`
+// and then generates a method to halve `my_var1` as `half_my_var1` in which
+// the caller can assume that they will inherit that function without impl it themselves
+#[proc_macro_derive(MyProcMacro)]
 pub fn my_proc_macro_fn(input: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
     let input = parse_macro_input!(input as DeriveInput);
 
-    // Build the output, possibly using quasi-quotation
+    // Check if the struct has a field named "my_var1" of type "f32"
+    let my_var1_exists = match input.data {
+        syn::Data::Struct(ref data) => data.fields.iter().any(|field| {
+            field
+                .ident
+                .as_ref()
+                .map_or(false, |ident| ident == "my_var1")
+                && matches!(&field.ty, syn::Type::Path(p) if p.path.is_ident("f32"))
+        }),
+        _ => false,
+    };
+
+    if !my_var1_exists {
+        // Emit a helpful compiler error
+        return syn::Error::new_spanned(
+            &input,
+            "Struct must have a field named 'my_var1' of type 'f32'",
+        )
+        .to_compile_error()
+        .into();
+    }
+
+    // Ensure that the struct has a field named `my_var1` of type `f32`
+    let my_var1_type = quote! { f32 };
+    let struct_name = &input.ident;
+
+    // Generate the code to halve `my_var1`
     let expanded = quote! {
-        // ...
+        impl #struct_name {
+            pub fn half_my_var1(&mut self) {
+                self.my_var1 /= 2.0;
+            }
+        }
     };
 
     // Hand the output tokens back to the compiler
